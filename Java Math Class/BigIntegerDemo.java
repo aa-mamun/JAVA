fe5a2271a3fb907092f2c5e1aec2ffd205154feb
Abdullah Al Mamun <mamun.ofcpro@gmail.com>
2017-05-20 15:50:36 +0600

    Add files via upload

---
+++ Java Math Class/BigIntegerDemo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author MAMUN
+ * */
+import java.math.*;
+
+public class BigIntegerDemo {
+
+   public static void main(String[] args) {
+
+	// create 4 BigInteger objects
+	BigInteger bi1, bi2, bi3, bi4,bi5,bi6;  
+
+	// assign values to bi1, bi2
+	bi1 = new BigInteger("123");
+	bi2 = new BigInteger("-123");
+        bi5 =new BigInteger("50");
+
+	// assign absolute values of bi1, bi2 to bi3, bi4
+	bi3 = bi1.abs();
+	bi4 = bi2.abs();
+	  
+	String str1 = "Absolute value of " + bi1 + " is " + bi3;
+	String str2 = "Absolute value of " + bi2 + " is " + bi4;
+
+	// print bi3, bi4 values
+	System.out.println( str1 );
+	System.out.println( str2 );
+        
+        bi6=bi1.add(bi5);
+        System.out.println( "REsult is "   +bi6 );
+   }
+}
---
+++ Java Math Class/GCD.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author MAMUN
+ */
+import java.math.*;
+
+public class GCD {
+
+public static void main(String[] args) {
+
+        // create 3 BigInteger objects
+	BigInteger bi1, bi2, bi3;
+
+	// assign values to bi1, bi2
+	bi1 = new BigInteger("18");
+	bi2 = new BigInteger("24");
+
+	// assign gcd of bi1, bi2 to bi3
+	bi3 = bi1.gcd(bi2);
+
+	String str = "GCD of " + bi1 + " and " + bi2 + " is " +bi3;
+
+	// print bi3 value
+	System.out.println( str );
+        
+        bi3 = bi1.divide(new BigInteger("3")); 
+        System.out.println( "Division result is " +bi3 );
+    }
+}
