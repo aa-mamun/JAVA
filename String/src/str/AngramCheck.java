70e8cebfd6f6681cf11a7656b17e3c4b4abb8f37
Abdullah Al Mamun <mamun.ofcpro@gmail.com>
2019-07-30 23:07:54 +0600

    refactor

--- String/src/str/AngramCheck.java
+++ String/src/str/AngramCheck.java
@@ -3,11 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practice;
+package str;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
+/** 
+ * An anagram of a string is another string that contains same characters, 
+ * only the order of characters can be different. 
+ * For example, “abcd” and “dabc” are anagram of each other.
+ */
 /**
  *
  * @author MAMUN
--- String/src/str/ReverseStringKeepingSpaceFixedInPos.java
+++ String/src/str/ReverseStringKeepingSpaceFixedInPos.java
@@ -1,5 +1,5 @@
 
-package practice;
+package str;
 
 import java.util.Arrays;
 import java.util.Scanner;
